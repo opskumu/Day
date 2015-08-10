9a996b471cc0101984676a8ab2f00cd6bfb64af3
opskumu <opskumu@gmail.com>
2015-08-10 10:19:43 +0800

    新增 go 学习.

---
+++ gostudy/gobyexample/variables/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := "initial"
+	fmt.Println(a)
+
+	b, c := 1, 2
+	fmt.Println(b, c)
+
+	d := true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "short"
+	fmt.Println(f)
+}
